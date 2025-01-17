d3949610c12ed625b441277dcd3f731e3ceaf4a5
arinrahman <rahman.arin29@gmail.com>
2025-01-17 14:54:27 -0700

    Printing to the Console in Python, String Manipulation and Code Intelligence, Input Function, Variables

---
+++ task.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+#to print in MAC just press Control + '+' + R
+# 'Hello World!' is a string
+print("Hello!\nHello!")
+
+#back slash in MAC Alt + Control + "/"
+
+#this is concatenation
+print("Arin"+ "Anmol")
+
+print("Arin" + " " + "Anmol")
+
+#the input function takes some data from the user
+
+val= input("What's your name?")
+print("The user's name is " + val)
+
+print('Hello ' + input("What's your name?") + " !")
+
+i= input("Type a word to know its length.")
+l= str(len(i))
+print("The length of the word " +i + " is " + l)
